9c8066f5eddddd5e036701913a9787a0205e364e
alacwong <mr6902spades@gmail.com>
2020-09-09 19:32:44 -0400

    (feat): Configure http

--- menu-app/src/app/app.module.ts
+++ menu-app/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { SectionComponent } from './section/section.component';
+import { MenuItemComponent } from './menu-item/menu-item.component';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, MenuComponent, SectionComponent, MenuItemComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
