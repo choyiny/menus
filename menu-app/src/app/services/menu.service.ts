e3ba0b1f7b58510ab957fbb34640fb2118f11b6f
alacwong <mr6902spades@gmail.com>
2020-09-11 11:35:18 -0400

    refactor: replaced hardcoded url with env variable

--- menu-app/src/app/services/menu.service.ts
+++ menu-app/src/app/services/menu.service.ts
@@ -1,19 +1,19 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Observable} from 'rxjs';
 import { MenuInterface } from '../interfaces/MenuInterface';
-import { SectionInterface } from '../interfaces/SectionIterface';
-import { MenuItemInterface } from '../interfaces/MenuItemInterface';
-import { TagInterface } from '../interfaces/TagInterface';
+import {environment} from '../../environments/environment';
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class MenuService {
-  private url = 'http://127.0.0.1:5000/menus/';
+
+  private url: string = environment.menu_api;
 
   constructor(private http: HttpClient) {}
   getMenu(slug: string): Observable<MenuInterface> {
-    return this.http.get<MenuInterface>(this.url + `/${slug}`);
+    return this.http.get<MenuInterface>(`${this.url}/${slug}`);
   }
 }
--- menu-app/src/environments/environment.prod.ts
+++ menu-app/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
   production: true,
+  menu_api: 'http://127.0.0.1:5000/menus'
 };
