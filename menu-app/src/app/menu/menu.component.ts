95618cf0de69beea8757e6941c62cae29edc2fa2
alacwong <mr6902spades@gmail.com>
2020-09-12 02:24:27 -0400

    fix: style changes to env:

--- menu-app/src/app/menu/menu.component.ts
+++ menu-app/src/app/menu/menu.component.ts
@@ -43,8 +43,13 @@ export class MenuComponent implements OnInit {
     const section = document.getElementById(id);
     const headerOffset = header.offsetHeight;
     const sectionPosition = section.getBoundingClientRect().top;
-    const offsetPosition = sectionPosition - headerOffset;
-    console.log({offsetPosition, headerOffset, sectionPosition, id});
+    let offsetPosition;
+    if (sectionPosition < 0) {
+      offsetPosition = sectionPosition + headerOffset;
+    } else {
+      offsetPosition = sectionPosition - headerOffset;
+    }
+    console.log({ offsetPosition, headerOffset, sectionPosition });
     window.scrollTo({
       top: offsetPosition,
     });
