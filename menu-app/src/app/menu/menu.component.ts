ea76da78d704ca44e26444ef249d2fbddaf67a9f
alacwong <mr6902spades@gmail.com>
2020-09-14 16:49:06 -0400

    fix: rounded border

--- menu-app/src/app/menu/menu.component.ts
+++ menu-app/src/app/menu/menu.component.ts
@@ -1,16 +1,29 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import {Component, HostListener, Input, OnInit} from '@angular/core';
 import { MenuInterface } from '../interfaces/MenuInterface';
 import { MenuService } from '../services/menu.service';
 import { ActivatedRoute, ParamMap } from '@angular/router';
+import {style, state, animate, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss'],
+  animations: [
+    trigger('fade', [
+      transition(':enter', [
+        style({opacity: 0}),
+        animate(500, style({opacity: 1}))
+      ]),
+      transition(':leave', [
+        animate(500, style({opacity: 0}))
+      ])
+    ])
+  ]
 })
 export class MenuComponent implements OnInit {
   menu: MenuInterface;
   showImage = true;
+  @Input()selectedSection: string;
 
   constructor(private menuservice: MenuService, private route: ActivatedRoute) {}
 
@@ -42,6 +55,10 @@ export class MenuComponent implements OnInit {
     window.scroll({top: 0});
     const headerOffset = document.getElementById('header').offsetHeight;
     const topOfElement = document.getElementById(id).getBoundingClientRect().top - headerOffset;
+    console.log(topOfElement);
     window.scroll({ top: topOfElement});
+    this.selectedSection = id;
+    console.log(this.selectedSection);
   }
+
 }
