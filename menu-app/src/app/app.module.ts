e2939e529a5b53c31d2c65a05c9287877bd665ca
alacwong <mr6902spades@gmail.com>
2020-09-14 17:00:04 -0400

    fix: update module for animations

--- menu-app/src/app/app.module.ts
+++ menu-app/src/app/app.module.ts
@@ -8,10 +8,12 @@ import { SectionComponent } from './section/section.component';
 import { MenuItemComponent } from './menu-item/menu-item.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FooterComponent } from './footer/footer.component';
+import { TagsComponent } from './tags/tags.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [AppComponent, MenuComponent, SectionComponent, MenuItemComponent, FooterComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  declarations: [AppComponent, MenuComponent, SectionComponent, MenuItemComponent, FooterComponent, TagsComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
