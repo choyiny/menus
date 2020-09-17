9be31ee142da8d12a68e9058c7bfb8bb8ac1dc9d
alacwong <mr6902spades@gmail.com>
2020-09-17 16:08:08 -0400

    fix: fixed bug where wrong description was returned

--- menu-api/blueprints/menus/documents/menu.py
+++ menu-api/blueprints/menus/documents/menu.py
@@ -70,6 +70,7 @@ class Menu(Document):
     # a list of menu items
 
     sections = ListField(EmbeddedDocumentField(Section))
+
     # a list of sections in order
 
     def sectionized_menu(self) -> dict:
@@ -78,7 +79,7 @@ class Menu(Document):
         """
         # loop through all menus and then bucket it into different sections
         section_to_items = defaultdict(list)
-        name_to_section = defaultdict(list)
+        name_to_section = {}
 
         for item in self.menu_items:
             for section in item.sections:
@@ -87,14 +88,18 @@ class Menu(Document):
         for section in self.sections:
             name_to_section[section.name] = section
 
+        print(name_to_section)
+
         # combine it with section data
         sectionized = []
         for section_name, list_of_items in section_to_items.items():
+            x = section_name in name_to_section
+            print(section_name, x)
             sectionized.append(
                 {
                     "name": section_name,
                     "menu_items": list_of_items,
-                    "description": name_to_section[section.name].description,
+                    "description": name_to_section[section_name].description,
                 }
             )
         print(sectionized)
