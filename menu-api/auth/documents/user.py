6e4842c0cc289d9e56c7041ec692be3d0290adb7
alacwong <mr6902spades@gmail.com>
2020-09-25 16:27:58 -0400

    feat: added permissions for auth decorator

--- menu-api/auth/documents/user.py
+++ menu-api/auth/documents/user.py
@@ -7,12 +7,13 @@ from werkzeug.security import generate_password_hash, check_password_hash
 class User(Document):
     username = StringField(required=True)
     password_hash = StringField(required=True)
-    menus = ListField(required=True)
-    isAdmin = BooleanField(required=True)
+    menus = ListField()
+    is_admin = BooleanField(required=True)
 
     @classmethod
     def create(cls, username: str, password: str):
-        return User(username=username, password_hash=cls.hash_password(password)).save()
+        print(username, password)
+        return User(username=username, password_hash=cls.hash_password(password), menus=[], is_admin=False).save()
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
