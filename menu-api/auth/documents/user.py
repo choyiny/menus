d9caceb41e580b48e71fa8ed132d6a9c5732ef03
alacwong <mr6902spades@gmail.com>
2020-09-25 14:33:15 -0400

    feat: modified auth to include permissions

--- menu-api/auth/documents/user.py
+++ menu-api/auth/documents/user.py
@@ -1,12 +1,14 @@
 from __future__ import annotations
 
-from mongoengine import Document, StringField
+from mongoengine import Document, StringField, ListField, BooleanField
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(Document):
     username = StringField(required=True)
     password_hash = StringField(required=True)
+    menus = ListField(required=True)
+    isAdmin = BooleanField(required=True)
 
     @classmethod
     def create(cls, username: str, password: str):
