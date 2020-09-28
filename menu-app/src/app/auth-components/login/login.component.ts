97c324ee1b559f0b877eb1c0af40afe476a7b57e
alacwong <mr6902spades@gmail.com>
2020-09-28 12:45:02 -0400

    feat: added logic to contain login credentials

--- menu-app/src/app/auth-components/login/login.component.ts
+++ menu-app/src/app/auth-components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,7 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  loginForm = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+  });
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  onSubmit(): void {}
 }
