e62762e462ac4f5c5dba4401c28692152970b4da
alacwong <mr6902spades@gmail.com>
2020-10-02 15:58:22 -0400

    feat: updated patch to update link adn external link given non-empty

--- menu-api/blueprints/menus/controllers/menus_resources.py
+++ menu-api/blueprints/menus/controllers/menus_resources.py
@@ -158,6 +158,12 @@ class MenuResource(MenusBaseResource):
         if kwargs.get("description"):
             menu.description = kwargs["description"]
 
+        if kwargs.get('external_link') and kwargs.get('external_link') != '':
+            menu.external_link = kwargs.get('external_link')
+
+        if kwargs.get('link_name') and kwargs.get('link_name') != '':
+            menu.link_name = kwargs.get('link_name')
+
         return menu.save()
 
     @marshal_with(ErrorResponseSchema, code=404)
