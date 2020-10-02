98b2b171cc1cc75b3ca6d43a1fa151b62651fbb0
alacwong <mr6902spades@gmail.com>
2020-10-02 12:50:12 -0400

    feat: generated image-form module

---
+++ menu-app/src/app/util-components/image-form/image-form.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { ImageCroppedEvent } from 'ngx-image-cropper';
+
+@Component({
+  selector: 'app-image-form',
+  templateUrl: './image-form.component.html',
+  styleUrls: ['./image-form.component.scss'],
+})
+export class ImageFormComponent implements OnInit {
+  imageChangedEvent: any = '';
+  croppedImage: any = '';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  fileChangeEvent(event: any): void {
+    this.imageChangedEvent = event;
+  }
+  imageCropped(event: ImageCroppedEvent): void {
+    this.croppedImage = event.base64;
+    console.log(event);
+  }
+  imageLoaded(): void {
+    // show cropper
+    console.log('loaded');
+  }
+  cropperReady(): void {
+    // cropper ready
+    console.log('ready');
+  }
+  loadImageFailed(): void {
+    // show message
+  }
+}
