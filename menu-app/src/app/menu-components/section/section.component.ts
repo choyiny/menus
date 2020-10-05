d4090b8d9d9ad3af455bdfec587e7ac8a9354369
alacwong <mr6902spades@gmail.com>
2020-10-05 13:35:12 -0400

    feat: add drag and drop

--- menu-app/src/app/menu-components/section/section.component.ts
+++ menu-app/src/app/menu-components/section/section.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { SectionInterface } from '../../interfaces/section-interface';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { MenuItemInterface } from '../../interfaces/menu-item-interface';
 
 @Component({
   selector: 'app-section',
@@ -14,5 +16,19 @@ export class SectionComponent implements OnInit {
 
   ngOnInit(): void {
     this.descriptions = this.section.description.split('^');
+  }
+
+  drop(event: CdkDragDrop<MenuItemInterface[]>): void {
+    console.log('drop');
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
   }
 }
