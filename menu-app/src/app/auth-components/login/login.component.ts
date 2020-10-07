9009b9fd91a80a0a3ccc611ab278695bec104b85
alacwong <mr6902spades@gmail.com>
2020-10-07 17:41:59 -0400

    feat: change auth to firebase

--- menu-app/src/app/auth-components/login/login.component.ts
+++ menu-app/src/app/auth-components/login/login.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { auth } from 'firebase/app';
 
 @Component({
   selector: 'app-login',
@@ -10,31 +8,19 @@ import { auth } from 'firebase/app';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  loginForm;
-  error: string;
-  loggedIn = false;
+  constructor(private authService: AuthService, public angularAuth: AngularFireAuth) {}
 
-  constructor(
-    private authService: AuthService,
-    private fb: FormBuilder,
-    public angularAuth: AngularFireAuth
-  ) {}
-
-  ngOnInit(): void {
-    this.loginForm = this.fb.group({
-      username: [''],
-      password: [''],
-    });
-  }
-
-  onSubmit(): void {
-    const username = this.loginForm.value.username;
-    const password = this.loginForm.value.password;
-    this.authService.login(username, password);
-  }
+  ngOnInit(): void {}
 
   login(): void {
-    this.angularAuth.signInWithPopup(new auth.GoogleAuthProvider());
+    this.authService.login().subscribe(
+      (user) => {
+        console.log(user);
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
   }
   logout(): void {
     this.angularAuth.signOut();
--- menu-app/src/app/services/auth.service.ts
+++ menu-app/src/app/services/auth.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { UserInterface } from '../interfaces/user-interface';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, from } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { map, flatMap } from 'rxjs/operators';
 import { environment } from '../../environments/environment';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { auth } from 'firebase/app';
@@ -25,22 +25,24 @@ export class AuthService {
     return this.currentUserSubject.value;
   }
 
-  async login(username: string, password: string): Observable<UserInterface> {
-    const credentials = await this.authFireBase.signInWithPopup(new auth.GoogleAuthProvider());
-    return this.http
-      .post<any>(
-        `${environment.settings.endpoint}/auth/`,
-        { credentials },
-        { headers: { 'Content-Type': 'application/json' } }
-      )
-      .pipe(
-        map((user) => {
-          user.authdata = window.btoa(username + ':' + password);
-          sessionStorage.setItem('currentUser', JSON.stringify(user));
-          this.currentUserSubject.next(user);
-          return user;
-        })
-      );
+  public getUserIdToken(): any {
+    return this.authFireBase.idToken;
+  }
+
+  login(): Observable<UserInterface> {
+    const firebaseObservable = from(
+      this.authFireBase.signInWithPopup(new auth.GoogleAuthProvider())
+    );
+    return firebaseObservable.pipe(
+      flatMap((userCredentials) => {
+        return this.http.post<any>(`${environment.settings.endpoint}/auth/`, {}).pipe(
+          map((user) => {
+            sessionStorage.setItem('currentUser', JSON.stringify(user));
+            return user;
+          })
+        );
+      })
+    );
   }
 
   logout(): void {
