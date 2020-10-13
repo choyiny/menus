2af325af376a529ea316559cf45087b0ae53b016
alacwong <mr6902spades@gmail.com>
2020-10-13 13:59:54 -0400

    feat: added menu admin  component (import, delete, qr)

---
+++ menu-app/src/app/admin-components/menu-dashboard/menu-dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-dashboard',
+  templateUrl: './menu-dashboard.component.html',
+  styleUrls: ['./menu-dashboard.component.scss'],
+})
+export class MenuDashboardComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ menu-app/src/app/interfaces/menu-admin-interface.ts
@@ -0,0 +1,4 @@
+export interface MenuAdminInterface {
+  name: string;
+  slug: string;
+}
