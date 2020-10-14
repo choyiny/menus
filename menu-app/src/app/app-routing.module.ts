73ac9aeeab94c28367b544f8e5134ac7126a9889
alacwong <mr6902spades@gmail.com>
2020-10-14 12:24:59 -0400

    fix: refactored guard and changed router link

--- menu-app/src/app/app-routing.module.ts
+++ menu-app/src/app/app-routing.module.ts
@@ -5,16 +5,16 @@ import { LoginComponent } from './auth-components/login/login.component';
 import { DashboardComponent } from './auth-components/dashboard/dashboard.component';
 import { MenuDashboardComponent } from './admin-components/menu-dashboard/menu-dashboard.component';
 import { CreateComponent } from './admin-components/create/create.component';
-import { AuthGuardService as AuthGuard } from './services/auth-guard.service';
+import { AdminGuard } from './guards/admin.guard';
 import { AdminDashboardComponent as Admin } from './admin-components/admin-dashboard/admin-dashboard.component';
 
 const routes: Routes = [
   { path: 'menu/:slug', component: MenuComponent },
   { path: '', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'admin/menu', component: MenuDashboardComponent, canActivate: [AuthGuard] },
-  { path: 'admin/create', component: CreateComponent, canActivate: [AuthGuard] },
-  { path: 'admin', component: Admin, canActivate: [AuthGuard] },
+  { path: 'admin/menu', component: MenuDashboardComponent, canActivate: [AdminGuard] },
+  { path: 'admin/create', component: CreateComponent, canActivate: [AdminGuard] },
+  { path: 'admin', component: Admin, canActivate: [AdminGuard] },
   { path: '**', redirectTo: '' },
 ];
 
