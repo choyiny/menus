a7b2d66a8302f6a77e2bb046c9108107d75fa3c4
alacwong <mr6902spades@gmail.com>
2020-10-14 13:19:09 -0400

    feat: generated modal

---
+++ menu-app/src/app/util-components/covid-modal/covid-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-covid-modal',
+  templateUrl: './covid-modal.component.html',
+  styleUrls: ['./covid-modal.component.scss'],
+})
+export class CovidModalComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
