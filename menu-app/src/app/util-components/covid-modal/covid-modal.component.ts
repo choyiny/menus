a1d957d9ca7ed3ae1126687e9c08cbe61966146a
alacwong <mr6902spades@gmail.com>
2020-10-15 14:15:08 -0400

    feat: modal now exits once submitted

--- menu-app/src/app/util-components/covid-modal/covid-modal.component.ts
+++ menu-app/src/app/util-components/covid-modal/covid-modal.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { FormGroup, FormBuilder } from '@angular/forms';
+import { TracingService } from '../../services/tracing.service';
+import { MenuInterface } from '../../interfaces/menus-interface';
 
 @Component({
   selector: 'app-covid-modal',
@@ -10,8 +12,13 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 export class CovidModalComponent implements OnInit {
   @ViewChild('modalContent') input;
   tracingForm: FormGroup;
+  @Input() menu: MenuInterface;
 
-  constructor(private modalService: NgbModal, private fb: FormBuilder) {}
+  constructor(
+    private modalService: NgbModal,
+    private fb: FormBuilder,
+    private tracingService: TracingService
+  ) {}
 
   ngOnInit(): void {
     this.tracingForm = this.fb.group({
@@ -24,5 +31,15 @@ export class CovidModalComponent implements OnInit {
     this.modalService.open(this.input);
   }
 
-  submit(): void {}
+  submit(): void {
+    const contact = {
+      name: this.tracingForm.value.name,
+      phone_number: `+1${this.tracingForm.value.phone_number}`,
+      key: this.menu.tracing_key,
+    };
+    this.tracingService.traceCustomer(this.menu.name, contact).subscribe((timeIn) => {
+      localStorage.setItem('time_in', JSON.stringify(timeIn));
+      this.modalService.dismissAll();
+    });
+  }
 }
