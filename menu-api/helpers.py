703e9b273afe8d11da0913fbeeb48123737b2a7a
alacwong <mr6902spades@gmail.com>
2020-10-21 12:23:32 -0400

    refactor: move to helper

--- menu-api/helpers.py
+++ menu-api/helpers.py
@@ -25,3 +25,7 @@ def upload_image(file):
         ContentType="image/png",
     )
     return f"https://{config.S3_BUCKET_NAME}.s3.{config.AWS_REGION}.amazonaws.com/{filename}"
+
+
+def delete_file(file_name):
+    s3.delete_object(Bucket=config.S3_BUCKET_NAME, Key=f"media/{file_name}")
