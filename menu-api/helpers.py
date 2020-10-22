4af52cd290fd46159eea6620be15d4ba62401f8a
alacwong <mr6902spades@gmail.com>
2020-10-22 11:39:05 -0400

    doc: explanation for magic number

--- menu-api/helpers.py
+++ menu-api/helpers.py
@@ -28,4 +28,7 @@ def upload_image(file):
 
 
 def delete_file(image_url):
+    # last 40 characters is {uuid.png}, filename
+    # example
+    # https://pickeasy-restaurant-images.s3.us-east-1.amazonaws.com/0b1b04d7-b410-44b8-92b9-30dfa4c8a45b.png
     s3.delete_object(Bucket=config.S3_BUCKET_NAME, Key=image_url[-40:])
