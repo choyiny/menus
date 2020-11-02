6ec0918b4ad301396944396a792739f5492a32eb
alacwong <mr6902spades@gmail.com>
2020-11-02 11:36:32 -0500

    refactor: changed documentation format and moved menu document from restaurant.py

---
+++ menu-api/blueprints/restaurant/documents/menu.py
@@ -0,0 +1,111 @@
+from typing import Optional
+
+from mongoengine import (
+    Document,
+    EmbeddedDocument,
+    EmbeddedDocumentField,
+    ListField,
+    StringField,
+    URLField,
+)
+
+
+class Tag(EmbeddedDocument):
+    """
+    A tag for an item.
+    """
+
+    icon = StringField(required=True)
+    """
+    icon code for this tag
+    """
+
+    text = StringField(required=True)
+    """
+    text for this tag
+    """
+
+
+class Item(EmbeddedDocument):
+    """
+    A menu item, contained in a menu.
+    """
+
+    image = URLField()
+    """
+    image url of this menu item
+    """
+
+    name = StringField(required=True, default="No name")
+    """
+    name of this menu item
+    """
+
+    price = StringField(default="")
+    """
+    price of this menu item
+    """
+
+    tags = ListField(EmbeddedDocumentField(Tag), default=[])
+    """
+    list of tags for this menu item
+    """
+
+    description = StringField(default="No description")
+    """
+    description of this menu item
+    """
+
+    _id = StringField()
+    """
+    unique if of current menu item
+    """
+
+    def __eq__(self, other):
+        return type(self) == type(other) and self._id == other._id
+
+
+class Section(EmbeddedDocument):
+    _id = StringField(required=True)
+    name = StringField(required=True, default="New Section")
+    description = StringField(default="No description")
+    subtitle = StringField()
+    menu_items = ListField(EmbeddedDocumentField(Item))
+
+    def __eq__(self, other):
+        return type(self) == type(other) and self._id == other._id
+
+
+class Menu(Document):
+    """
+    A menu object.
+    """
+
+    name = StringField()
+    """
+    name of current menu
+    """
+
+    sections = ListField(EmbeddedDocumentField(Section))
+    """
+    List of ordered sections
+    """
+
+    def get_section(self, section_id) -> Optional[Section]:
+        """ get section of this menu """
+        for section in self.sections:
+            if section._id == section_id:
+                return section
+        return None
+
+    def get_item(self, section_id, item_id) -> Optional[Item]:
+        """ get menu-item from this menu """
+        for section in self.sections:
+            if section._id == section_id:
+                for item in section.menu_items:
+                    if item._id == item_id:
+                        return item
+        return None
+
+    def __eq__(self, other):
+        return type(self) == type(other) and self._id == other._id
