f0b2fb71ac62eedc19811d35e64912418dc75132
Daniel <niellxu@yahoo.ca>
2020-11-01 21:39:41 -0500

    Revert menu module

--- menu-app/src/app/app-routing.module.ts
+++ menu-app/src/app/app-routing.module.ts
@@ -3,8 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './auth-components/login/login.component';
 import { DashboardComponent } from './auth-components/dashboard/dashboard.component';
 import { AdminGuard } from './guards/admin.guard';
+import { MenuComponent } from './menu-components/menu/menu.component';
 
 const routes: Routes = [
+  {
+    path: 'menu/:slug',
+    component: MenuComponent
+  },
   {
     path: '',
     component: LoginComponent
@@ -12,12 +17,6 @@ const routes: Routes = [
   {
     path: 'dashboard',
     component: DashboardComponent
-  },
-  {
-    path: 'menu',
-    loadChildren: () =>
-      import('./menu-components/menu.module').then((module) => module.MenuModule),
-    canActivate: [AdminGuard],
   },
   {
     path: 'admin',
--- menu-app/src/app/app.module.ts
+++ menu-app/src/app/app.module.ts
@@ -15,6 +15,19 @@ import { AuthenticationInterceptor } from './interceptor/authentication.intercep
 import { ImageFormComponent } from './util-components/image-form/image-form.component';
 import { LoginComponent } from './auth-components/login/login.component';
 import { DashboardComponent } from './auth-components/dashboard/dashboard.component';
+import { MenuComponent } from './menu-components/menu/menu.component';
+import { MenuItemComponent } from './menu-components/menu-item/menu-item.component';
+import { SectionComponent } from './menu-components/section/section.component';
+import { TagsComponent } from './menu-components/tags/tags.component';
+import { ScrollingComponentComponent } from './util-components/scrolling-component/scrolling-component.component';
+import { MobileImageComponent } from './util-components/menu-util/mobile-image/mobile-image.component';
+import { CovidModalComponent } from './util-components/modals/covid-modal/covid-modal.component';
+import { ChangeBackgroundComponent } from './util-components/menu-util/change-background/change-background.component';
+import { MenuNameComponent } from './util-components/menu-util/menu-name/menu-name.component';
+import { MenuDetailsComponent } from './util-components/menu-util/menu-details/menu-details.component';
+import { FooterComponent } from './util-components/footer/footer.component';
+import { ImgFormModalComponent } from './util-components/modals/img-form-modal/img-form-modal.component';
+import { ImgViewModalComponent } from './util-components/modals/img-view-modal/img-view-modal.component';
 
 // NPM packages
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
@@ -23,6 +36,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { environment } from '../environments/environment';
 import { ImageCropperModule } from 'ngx-image-cropper';
 import { AngularFireModule } from '@angular/fire';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { QuillModule } from 'ngx-quill';
 
 @NgModule({
   declarations: [
@@ -30,6 +45,19 @@ import { AngularFireModule } from '@angular/fire';
     LoginComponent,
     ImageFormComponent,
     DashboardComponent,
+    MenuComponent,
+    MenuItemComponent,
+    SectionComponent,
+    TagsComponent,
+    ScrollingComponentComponent,
+    MobileImageComponent,
+    CovidModalComponent,
+    ChangeBackgroundComponent,
+    MenuNameComponent,
+    MenuDetailsComponent,
+    FooterComponent,
+    ImgViewModalComponent,
+    ImgFormModalComponent,
   ],
   imports: [
     BrowserModule,
@@ -43,6 +71,20 @@ import { AngularFireModule } from '@angular/fire';
     ImageCropperModule,
     FormsModule,
     AngularFireModule.initializeApp(environment.settings.firebase),
+    DragDropModule,
+    QuillModule.forRoot({
+      modules: {
+        toolbar: [
+          ['bold', 'italic', 'underline', 'strike'],
+          [{ list: 'ordered' }, { list: 'bullet' }],
+          [{ size: ['small', false, 'large', 'huge'] }],
+          [{ color: [] }],
+          [{ font: [] }],
+          [{ align: [] }],
+          ['link'],
+        ],
+      },
+    }),
   ],
   providers: [{ provide: HTTP_INTERCEPTORS, useClass: AuthenticationInterceptor, multi: true }],
   bootstrap: [AppComponent],
