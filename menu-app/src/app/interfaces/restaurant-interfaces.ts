38dd43027cbc025eec45bc3b9a95ecb11807da8f
alacwong <mr6902spades@gmail.com>
2020-11-03 15:34:36 -0500

    feat: added new interfaces associated with apiv2

---
+++ menu-app/src/app/interfaces/restaurant-interfaces.ts
@@ -0,0 +1,37 @@
+export interface Restaurant {
+  image ?: string;
+  name ?: string;
+  menus: string [];
+  description ?: string;
+  tracing_key: string;
+  enable_trace: boolean;
+  force_trace: boolean;
+}
+
+export interface Tag {
+  text: string;
+  icon: string;
+  background_color: string;
+}
+
+export interface Item {
+  name?: string;
+  price?: string;
+  description?: string;
+  _id: string;
+  image?: string;
+  tags: Tag[];
+}
+
+export interface Section {
+  name?: string;
+  description?: string;
+  subtitle?: string;
+  menu_items: Item [];
+  _id: string;
+}
+
+export interface Menu {
+  name: string;
+  sections: Section[];
+}
