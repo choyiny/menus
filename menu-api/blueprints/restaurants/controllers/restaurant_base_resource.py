68ef2081427056dccbbac07ca406b913eca2ad64
alacwong <mr6902spades@gmail.com>
2020-11-04 17:36:12 -0500

    fix: correct the error codes

--- menu-api/blueprints/restaurants/controllers/restaurant_base_resource.py
+++ menu-api/blueprints/restaurants/controllers/restaurant_base_resource.py
@@ -5,7 +5,8 @@ from helpers import BaseResource, ErrorResponseSchema
 @doc(tags=["Restaurant"])
 @marshal_with(ErrorResponseSchema, code=404)
 @marshal_with(ErrorResponseSchema, code=422)
-@marshal_with(ErrorResponseSchema, code=412)
+@marshal_with(ErrorResponseSchema, code=409)
 @marshal_with(ErrorResponseSchema, code=401)
+@marshal_with(ErrorResponseSchema, code=403)
 class RestaurantBaseResource(BaseResource):
     pass
--- menu-api/utils/errors.py
+++ menu-api/utils/errors.py
@@ -8,9 +8,11 @@ RESTAURANT_NOT_FOUND = {"description": "Restaurant not found."}, 404
 
 IMAGE_NOT_FOUND = {"description": "Image not found"}, 404
 
-FORBIDDEN = {"description": "You do not have permission"}, 401
+FORBIDDEN = {"description": "You do not have permission"}, 403
 
-MENU_ALREADY_EXISTS = {"description": "Menu already exists"}, 400
+NOT_AUTHENTICATED = {"description": "You are not authenticated"}, 401
+
+MENU_ALREADY_EXISTS = {"description": "Menu already exists"}, 409
 
 USER_ALREADY_EXISTS = {"description": "User already exists with that email"}, 400
 
