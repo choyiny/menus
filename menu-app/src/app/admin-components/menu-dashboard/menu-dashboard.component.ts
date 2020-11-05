9aedca8acb3367cab8abd20d9f7bf67a049ec7b4
alacwong <mr6902spades@gmail.com>
2020-11-05 18:45:01 -0500

    feat: implement menu import/delete/append

--- menu-app/src/app/admin-components/menu-dashboard/menu-dashboard.component.ts
+++ menu-app/src/app/admin-components/menu-dashboard/menu-dashboard.component.ts
@@ -3,8 +3,11 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { MenuService } from '../../services/menu.service';
 import * as FileSaver from 'file-saver';
 import { MenuInterface } from '../../interfaces/menus-interface';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import {Form, FormBuilder, FormGroup} from '@angular/forms';
 import { TracingService } from '../../services/tracing.service';
+import {Restaurant} from "../../interfaces/restaurant-interfaces";
+import {RestaurantService} from "../../services/restaurant.service";
+import {AdminService} from "../../services/admin.service";
 
 @Component({
   selector: 'app-menu-dashboard',
@@ -14,69 +17,102 @@ import { TracingService } from '../../services/tracing.service';
 export class MenuDashboardComponent implements OnInit {
   constructor(
     private router: Router,
-    private menuService: MenuService,
+    private restaurantService: RestaurantService,
+    private adminService: AdminService,
     private route: ActivatedRoute,
     private fb: FormBuilder,
     private tracingService: TracingService
   ) {}
 
-  menu: MenuInterface;
+  restaurant: Restaurant;
   file: File;
-  baseUrl;
   slug: string;
   configureContactTracing = false;
   contactTracingForm: FormGroup;
+  selectedMenu: string;
+  menuBody: FormGroup;
 
   ngOnInit(): void {
     this.slug = this.route.snapshot.params.slug;
+    this.load();
+    this.menuBody = this.fb.group({
+      name: ['']
+    });
+  }
+
+  load(): void {
     if (this.slug != null) {
-      this.menuService.getMenu(this.slug).subscribe((menu) => {
-        this.menu = menu;
+      this.restaurantService.getRestaurant(this.slug).subscribe((restaurant) => {
+        this.restaurant = restaurant;
         this.contactTracingForm = this.fb.group({
-          enable_trace: [this.menu.enable_trace],
-          force_trace: [this.menu.force_trace],
-          tracing_key: [this.menu.tracing_key],
+          enable_trace: [this.restaurant.enable_trace],
+          force_trace: [this.restaurant.force_trace],
+          tracing_key: [this.restaurant.tracing_key],
         });
       });
     }
-    this.baseUrl = window.location.origin;
   }
 
   onChange(event): void {
     this.file = event.target.files[0];
   }
 
+  menuOptions(): string[] {
+    return ['Make new menus', ...this.restaurant.menus];
+  }
+
   importCsv(): void {
-    const formData = new FormData();
-    formData.append('file', this.file);
-    this.menuService.uploadCsv(this.slug, formData).subscribe((menu) => {});
+    const name = this.menuBody.value.name;
+    if (name) {
+      const formData = new FormData();
+      formData.append('file', this.file);
+      this.adminService.importMenu(this.slug, name, formData).subscribe(
+        restaurant => {
+          this.load();
+          window.alert('Success!');
+        },
+        err => {
+          window.alert('Something went wrong!');
+          console.log(err);
+        }
+      );
+    } else {
+      window.alert('enter a name first');
+    }
   }
 
   appendCsv(): void {
     const formData = new FormData();
     formData.append('file', this.file);
-    this.menuService.appendCsv(this.slug, formData).subscribe((menu) => {});
+    this.adminService.appendMenu(this.slug, this.selectedMenu, formData).subscribe((menu) => {
+      window.alert('Success!');
+    });
   }
 
   deleteMenu(): void {
-    this.menuService.deleteMenu(this.slug).subscribe((menu) => {});
+    this.restaurantService.deleteMenu(this.slug, this.selectedMenu).subscribe(
+      menu => {
+        this.load();
+        window.alert('Success!');
+      }
+    );
   }
 
   generateQr(): void {
     let body;
-    if (this.menu.enable_trace) {
+    if (this.restaurant.enable_trace) {
       body = {
-        url: `${this.baseUrl}/menu/${this.slug}?trace=true`,
-        name: this.menu.name,
+        url: `${window.location.origin}/menu/${this.slug}?trace=true`,
+        name: this.restaurant.name,
       };
     } else {
       body = {
-        url: `${this.baseUrl}/menu/${this.slug}`,
-        name: this.menu.name,
+        url: `${window.location.origin}/menu/${this.slug}`,
+        name: this.restaurant.name,
       };
     }
     this.menuService.generateQR(body).subscribe((blob) => {
-      const fileName = `${this.menu.name}.${blob.type}`;
+      const fileName = `${this.restaurant.name}.${blob.type}`;
       FileSaver.saveAs(blob, fileName);
     });
   }
@@ -88,7 +124,7 @@ export class MenuDashboardComponent implements OnInit {
   submitContactTracing(): void {
     const tracingForm = this.contactTracingForm.value;
     this.tracingService.configureTracing(this.slug, tracingForm).subscribe((menu) => {
-      this.menu = menu;
+      this.restaurant = menu;
     });
     this.configureContactTracing = false;
   }
