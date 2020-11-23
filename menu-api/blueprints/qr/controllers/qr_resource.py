86d3b3d61db46bb141c07ee84aa4b511a0e0e0b6
alacwong <mr6902spades@gmail.com>
2020-11-23 12:27:50 -0500

    fix: potential error 500 when restaruant not found

--- menu-api/blueprints/qr/controllers/qr_resource.py
+++ menu-api/blueprints/qr/controllers/qr_resource.py
@@ -1,5 +1,6 @@
 from flask import redirect
 from flask_apispec import doc
+from utils.errors import RESTAURANT_NOT_FOUND
 
 from ...restaurants.documents.restaurant import Restaurant
 from .qr_base_resource import QrBaseResource
@@ -9,6 +10,8 @@ class DynamicQrResource(QrBaseResource):
     @doc(description="""redirect user to restaurant's qrcode link""")
     def get(self, slug):
         restaurant = Restaurant.objects(slug=slug).first()
+        if restaurant is None:
+            return RESTAURANT_NOT_FOUND
         if restaurant.qrcode_link:
             return redirect(restaurant.qrcode_link, 301)
         else:
