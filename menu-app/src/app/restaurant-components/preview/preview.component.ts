9bf3c8a2a9731658a2238e76b43e794343e8feff
alacwong <mr6902spades@gmail.com>
2020-11-24 12:32:47 -0500

    fix: back to editing, when not editing

--- menu-app/src/app/restaurant-components/preview/preview.component.ts
+++ menu-app/src/app/restaurant-components/preview/preview.component.ts
@@ -1,19 +1,18 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {Restaurant} from '../../interfaces/restaurant-interfaces';
-import {DomSanitizer, SafeResourceUrl} from "@angular/platform-browser";
+import { Component, Input, OnInit } from '@angular/core';
+import { Restaurant } from '../../interfaces/restaurant-interfaces';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-preview',
   templateUrl: './preview.component.html',
-  styleUrls: ['./preview.component.scss']
+  styleUrls: ['./preview.component.scss'],
 })
 export class PreviewComponent implements OnInit {
-
-  @Input()restaurant: Restaurant;
-  @Input()slug: string;
+  @Input() restaurant: Restaurant;
+  @Input() slug: string;
   url: string;
 
-  constructor(private sanitizer: DomSanitizer) { }
+  constructor(private sanitizer: DomSanitizer) {}
 
   ngOnInit(): void {
     this.url = `${window.location.origin}/restaurants/${this.slug}`;
@@ -22,5 +21,4 @@ export class PreviewComponent implements OnInit {
   getUrl(): SafeResourceUrl {
     return this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
   }
-
 }
