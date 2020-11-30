4c72882d883ec91918c81ebae7deb27bca689e69
alacwong <mr6902spades@gmail.com>
2020-11-30 11:52:35 -0500

    fix: duct tape fix section empty

--- menu-app/src/app/control-panel/collapsed-section/collapsed-section.component.ts
+++ menu-app/src/app/control-panel/collapsed-section/collapsed-section.component.ts
@@ -36,6 +36,9 @@ export class CollapsedSectionComponent implements OnInit {
 
   edit(): void {
     this.editMode = !this.editMode;
+    if (!this.section.name) {
+      this.section.name = 'Edit sections...';
+    }
     this.sectionOriginal = this.parse(this.section);
   }
 
@@ -46,5 +49,10 @@ export class CollapsedSectionComponent implements OnInit {
   cancel(): void {
     this.section = this.parse(this.sectionOriginal);
     this.editMode = false;
+  }
+
+  setFocus(editor): void {
+    console.log(editor);
+    editor.focus();
   }
 }
