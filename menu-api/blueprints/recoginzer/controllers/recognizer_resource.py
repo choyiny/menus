c1d3ee34fbcd1b4f7ce926a93b5528980e85236c
alacwong <mr6902spades@gmail.com>
2020-12-01 11:52:14 -0500

    feat: implement image upload endpoint

--- menu-api/blueprints/recoginzer/controllers/recognizer_resource.py
+++ menu-api/blueprints/recoginzer/controllers/recognizer_resource.py
@@ -1,11 +1,23 @@
-from flask_apispec import doc, marshal_with
+from blueprints.admin.schemas import file_args
+from flask_apispec import doc, marshal_with, use_kwargs
 from helpers import BaseResource, ErrorResponseSchema
+from marshmallow import Schema
+from webargs import fields
+from webargs.flaskparser import use_args
 
 
 @doc(tags=["Menu Recognizer"])
 @marshal_with(ErrorResponseSchema, code=401)
 @marshal_with(ErrorResponseSchema, code=404)
 class RecognizerResource(BaseResource):
+    class RecognizerSchema(Schema):
+        template = fields.Str(
+            description="Template type for type of algorithm to parse image",
+            example="Row",
+        )
+
     @doc(description="""Scan an image and return recognized text""")
-    def post(self):
+    @use_args(file_args, location="files")
+    @use_kwargs(RecognizerSchema, location="form")
+    def post(self, args, **kwargs):
         pass
