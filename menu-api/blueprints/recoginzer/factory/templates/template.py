5b3d69832448c1a47336da160b59083ab4adfba1
alacwong <mr6902spades@gmail.com>
2020-12-02 17:55:47 -0500

    feat: finish porting row template

--- menu-api/blueprints/recoginzer/factory/templates/template.py
+++ menu-api/blueprints/recoginzer/factory/templates/template.py
@@ -10,9 +10,6 @@ class BaseTemplate:
         """Get json result s"""
         pass
 
-    def __init__(self, content: bytes, **kwargs):
-        self.content = content
-
     def draw_image(self):
         pass
 
@@ -52,8 +49,8 @@ class RowTemplate(BaseTemplate):
 
         th = 2
         H, W = img.shape[:2]
-        uppers = [y for y in range(H - 1) if hist[y] <= th and hist[y + 1] > th]
-        lowers = [y for y in range(H - 1) if hist[y] > th and hist[y + 1] <= th]
+        uppers = [y for y in range(H - 1) if hist[y] <= th < hist[y + 1]]
+        lowers = [y for y in range(H - 1) if hist[y] > th >= hist[y + 1]]
 
         threshed = cv2.cvtColor(threshed, cv2.COLOR_GRAY2BGR)
         for y in uppers:
@@ -74,3 +71,37 @@ class RowTemplate(BaseTemplate):
     def get_result(self) -> dict:
         points = detect_text(self.content)[1:]
         uppers, lowers = self.split_lines(points)
+        lines = []
+        for i in range(len(uppers)):
+            upper_y = uppers[i] - self.lines_error
+            if i >= len(lowers):
+                break
+            else:
+                lower_y = lowers[i] + self.lines_error
+                inline_text = []
+                lines.append({"upper": upper_y, "lower": lower_y, "text": inline_text})
+                rest_data = []
+                for points_text in points:
+                    text = points_text["text"]
+                    points = points_text["point"]
+                    inline = True
+                    for p in points:
+                        y = p[1]
+                        if upper_y <= y <= lower_y:
+                            inline = True
+                        else:
+                            inline = False
+                            break
+                    if inline:
+                        inline_text.append(text)
+                    else:
+                        rest_data.append(points_text)
+                points = rest_data
+        return {"lines": lines, "left": points}
+
+    def __init__(self, content: bytes, **kwargs):
+        self.lines_error = 10
+        self.content = content
+
+        if kwargs.get("line_error"):
+            self.lines_error = kwargs.get("line_error")
