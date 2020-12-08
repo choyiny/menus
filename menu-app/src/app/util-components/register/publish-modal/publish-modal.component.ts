e0c0424397e24dc6b19c0e06e16569668155dfeb
alacwong <mr6902spades@gmail.com>
2020-12-08 15:11:39 -0500

    feat: implement confetti

--- menu-app/src/app/util-components/register/publish-modal/publish-modal.component.ts
+++ menu-app/src/app/util-components/register/publish-modal/publish-modal.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { faQrcode } from '@fortawesome/pro-solid-svg-icons';
+import * as confetti from 'canvas-confetti';
 
 @Component({
   selector: 'app-publish-modal',
@@ -19,5 +20,39 @@ export class PublishModalComponent implements OnInit {
 
   open(): void {
     this.modalService.open(this.publishModal);
+    this.launchFireworks();
+  }
+
+  launchFireworks(): void {
+    const duration = 5 * 1000;
+    const animationEnd = Date.now() + duration;
+    const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 10001 };
+
+    const randomInRange = (min, max) => {
+      return Math.random() * (max - min) + min;
+    };
+
+    const interval = setInterval(() => {
+      const timeLeft = animationEnd - Date.now();
+
+      if (timeLeft <= 0) {
+        return clearInterval(interval);
+      }
+
+      const particleCount = 50 * (timeLeft / duration);
+      // since particles fall down, start a bit higher than random
+      confetti.create(undefined, { resize: true, useWorker: true })(
+        Object.assign({}, defaults, {
+          particleCount,
+          origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+        })
+      );
+      confetti.create(undefined, { resize: true, useWorker: true })(
+        Object.assign({}, defaults, {
+          particleCount,
+          origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+        })
+      );
+    }, 250);
   }
 }
