287ac819e926211fbf09fb28b35df64303537885
alacwong <mr6902spades@gmail.com>
2020-12-08 11:39:58 -0500

    feat: use angular material for tooltip for faster hover

--- menu-app/src/app/app.module.ts
+++ menu-app/src/app/app.module.ts
@@ -53,6 +53,7 @@ import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
 import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
 import { MatListModule } from '@angular/material/list';
 import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 const DragConfig = {
   dragStartThreshold: 0,
@@ -106,6 +107,7 @@ const DragConfig = {
     NgxSkeletonLoaderModule.forRoot(),
     MatBottomSheetModule,
     DragDropModule,
+    MatTooltipModule,
     QuillModule.forRoot({
       modules: {
         toolbar: [
